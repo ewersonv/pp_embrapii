45045f143e71e36cd076ea487d149e0495b6834e
Ewerson Vieira <ewersonv@gmail.com>
2018-08-15 17:21:35 -0300

    Add files via upload

--- arquivos/Teste/index.php
+++ arquivos/Teste/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <DOCTYPE html>
 <html lang="pt-br">
 	<head>
@@ -6,6 +9,11 @@
 	</head>
 	<body>
 		<h1>Cadastrar Empresa</h1>
+		<?php
+		if(isset($_SESSION['msg']))
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+		?>
 		<form method="POST" action="processa.php">
 			<label>Nome da empresa: </label>
 			<input type="text" name="nome" placeholder="Digite o nome da empresa"><br><br>
@@ -14,7 +22,7 @@
 			<input type="text" name="nome_rep" placeholder="Digite o nome do representante"><br><br>
 
 			<label>CNPJ: </label>
-			<input type="text" name="cnpj" placeholder="Digite o CNPJ da empresa"><br><br>
+			<input type="number" name="cnpj" placeholder="Apenas números"><br><br>
 
 			<label>Email: </label>
 			<input type="email" name="email" placeholder="Digite o email"><br><br>
