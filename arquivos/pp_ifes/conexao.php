296a70bee03512dd756e47cfd2b30f3b96f28d75
Ewerson Vieira <ewersonv@gmail.com>
2018-08-25 21:42:27 -0300

    Add files via upload

---
+++ arquivos/pp_ifes/conexao.php
@@ -0,0 +1,7 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "pp_ifes";
+//Criar conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
---
+++ arquivos/pp_ifes/processa.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include_once("conexao.php");
+
+$cb1 = filter_input(INPUT_POST, 'cb1', FILTER_SANITIZE_STRING);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_NUMBER_INT);
+
+//echo "Nome: $nome <br>";
+//echo "CNPJ: $cnpj <br>";
+//echo "Nome Representante: $nome_rep <br>";
+//echo "E-mail: $email <br>";
+
+$result_pessoa = "INSERT INTO pessoa (tipo_pessoa, nome, email, telefone, created) VALUES ('$cb1','$nome', '$email', $telefone, NOW())";
+$resultado_pessoa = mysqli_query($conn, $result_pessoa);
+
+/*if($_POST['checkbox'] != "")
+{
+	if(is_array($_POST['checkbox']))
+	{
+		while(list($key, $value) = each($_POST['checkbox']))
+		{
+			$sql = mysqli_query("INSERT INTO pessoa (tipo_pessoa) VALUES ('$value')");
+		}
+	}
+}*/
+
+if(mysqli_insert_id($conn))
+{
+	$_SESSION['msg'] = "<p style='color:green;'>Pessoa cadastrada com sucesso</p>";
+	header("Location: index.php");
+}
+else
+{
+	$_SESSION['msg'] = "<p style='color:red;'>Pessoa não foi cadastrada com sucesso</p>";
+	header("Location: index.php");
+}
