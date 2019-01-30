9b46889e2bc7fbebbcaed44af3eee7929cd43289
ewersonv <ewersonv@gmail.com>
2019-01-30 17:04:54 -0200

    change onclick to onsubmit index.php

--- arquivos/spp_ifes/view/index.php
+++ arquivos/spp_ifes/view/index.php
@@ -8,7 +8,7 @@ include_once("header.html");
 		<div class="container">
 			<div class="py-5 text-center">
 				<h1 class="mb-0">
-					<br><a class="text-dark">Pesquisar projetos</a><br><br>
+					<br><br><a class="text-dark">Pesquisar projetos</a><br><br>
 				</h1>
 			</div>
 				<div class="text-left">
@@ -21,7 +21,9 @@ include_once("header.html");
 						<input class="form-control mr-sm-2" type="text" name="nome_produto" style="max-width:400px;" placeholder="Pesquisar produto">
 					<label>Nome do projeto:</label>
 						<input class="form-control mr-sm-2" type="text" name="nome_projeto" style="max-width:400px;" placeholder="Pesquisar projeto">
-					<br><button class="btn btn-success my-2 my-sm-0" type="button" onclick="validateAndSend()">Pesquisar</button>
+					<div class="text-center">
+						<br><button class="btn btn-success my-2 my-sm-0" type="submit" onsubmit="validateAndSend()">Pesquisar</button>
+					</div>
 				</form>
 				</div>
 		</div>
