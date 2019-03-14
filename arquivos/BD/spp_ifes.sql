03a7ec05bad69004cad74000c123ada1f9470733
ewersonv <ewersonv@gmail.com>
2019-03-14 13:21:11 -0300

    add new BD

--- arquivos/BD/spp_ifes.sql
+++ arquivos/BD/spp_ifes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 13-Mar-2019 às 19:59
+-- Generation Time: 14-Mar-2019 às 16:18
 -- Versão do servidor: 5.7.23
 -- versão do PHP: 7.2.10
 
@@ -98,6 +98,7 @@ CREATE TABLE IF NOT EXISTS `projeto` (
   `custo` varchar(2000) NOT NULL,
   `interessados` varchar(2000) NOT NULL,
   `anotacoes_complementares` varchar(2000) NOT NULL,
+  `finalizado` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL,
   `fk_id_empresa` int(11) NOT NULL,
@@ -111,15 +112,15 @@ CREATE TABLE IF NOT EXISTS `projeto` (
 -- Extraindo dados da tabela `projeto`
 --
 
-INSERT INTO `projeto` (`id`, `nome`, `viabilidade`, `efeitos`, `equipe`, `riscos`, `entregas`, `premissas`, `cronograma`, `custo`, `interessados`, `anotacoes_complementares`, `created`, `modified`, `fk_id_empresa`, `fk_id_usuario`) VALUES
-(1, 'Novo tijolo refratario', 'Este projeto é viável por conta de ...', 'Os efeitos que poderão ser notados aaaa', 'Equipe projeto 1', 'Riscos 1 aaaa', 'As datas das entregas estão previstas para aaaa', 'O projeto parte das seguintes premissas aaaa', 'Cronograma projeto 1', 'O projeto tem um custo estimado de aaaaaaaaaa', 'As empresas interessadas sao aaa aaa aaa', 'Aqui entram as anotações complementares a respeito do projeto para explicações mais profundas sobre o que será feito', '2018-11-13 00:00:00', NULL, 1, 1),
-(2, 'Produto teste', 'A viabilidade deste projeto se dá por ...', 'Os efeitos que poderão ser notados BBBBbbbbBBBB', 'Equipe do projeto 2', 'Riscos 2 BBBBBB', 'As datas das entregas estão previstas para bbbbbbbbbbbb', 'O projeto parte das seguintes premissas BbBbBBbbB', 'Cronograma do projeto 2', 'O projeto tem um custo estimado de BBBBbbbbBBBBbbb', 'As empresas interessadas sao BBB bBB bbb Bbb', 'Aqui entram as anotações complementares a respeito do projeto 2 BBBBBbbBBBB', '2018-11-12 22:37:15', NULL, 2, 2),
-(3, 'Teste teste teste', 'O projeto é viável porque ...', 'Os efeitos que poderão ser notados CCCcccCCCcc', 'Equipe projeto 3CC', 'Riscos 3 CccCC', 'As datas das entregas estão previstas para CCCCCCCC', 'O projeto parte das seguintes premissas CCCCC', 'Cronograma projeto 1', 'O projeto tem um custo estimado de CCCCCccccCCCC', 'As empresas interessadas sao CCCCCCC', 'Anotações complementares a respeito do projeto 3 CCCC', '2018-11-12 22:39:14', NULL, 1, 3),
-(4, 'Nome do novo projeto 4', 'A viabilidade do projeto consiste em ...', 'Os efeitos que poderão ser notados 4444444', 'Equipe projeto 4 ddddd', 'Riscos 4 dddddd', 'As datas das entregas estão previstas para dddddddddddddddddddd', 'O projeto parte das seguintes premissas 44d4dd4d4', 'Cronograma do projeto 4', 'O projeto tem um custo estimado de 4444444444', 'As empresas interessadas sao 444 dddd DDD', 'Anotações e observações do projeto 4', '2018-11-12 22:41:21', NULL, 3, 1),
-(5, 'Mais um produto 55', 'Este projeto é viável por conta de ...', 'Os efeitos que poderão ser notados em 55555EEE', 'Equipe do projeto 5', 'Riscos 5 EEE', 'As datas das entregas estão previstas para 5555eeee', 'O projeto parte das seguintes premissas 55555eeee', 'Cronograma projeto 5E', 'O projeto tem um custo estimado de 55EEEEEEEE', 'As empresas interessadas sao 555 eeee eeee', 'Aqui entram as anotações complementares do projeto 5E', '2018-12-11 22:42:49', NULL, 1, 3),
-(6, 'Nome do projeto 6 XXXX', 'Viabilidade do projeto ...', 'Efeitos do projeto 6 FFFF', 'Equipe do projeto 6', 'Riscos do projeto 6 XXXx', 'Aqui entram as datas das entregas do projeto 6 XXXX', 'O projeto 6 parte das seguintes premissas 6666XXXXX', 'Cronograma do projeto 6x', 'O projeto 6 tem um custo estimado de 666666', 'As empresas interessadas no projeto 6 sao XXX xxx', 'Anotações e observações a respeito do projeto 6 XXX', '2018-12-13 22:48:27', NULL, 2, 3),
-(7, 'Projeto 7', 'Viabilidade do Projeto 7', 'Efeitos resultantes do Projeto 7', 'Equipe do Projeto 7', 'Riscos do Projeto 7', 'Entregas do Projeto 7', 'Premissas do Projeto 7', 'Cronograma de atividades do Projeto 7', 'Custo do Projeto 7', 'Empresas interessadas no Projeto 7', 'Informações adicionais do Projeto 7', '2019-01-14 22:52:37', NULL, 4, 4),
-(8, 'projeto 8', 'é viável', 'muitos efeitos legais', 'uma galera aí', 'um risco ou dois', 'vamos entregar', 'partimos do pressuposto que', 'tamo fazendo', 'bastante', 'algumas pessoas', 'tá bom já', '2019-03-11 23:05:52', NULL, 5, 1);
+INSERT INTO `projeto` (`id`, `nome`, `viabilidade`, `efeitos`, `equipe`, `riscos`, `entregas`, `premissas`, `cronograma`, `custo`, `interessados`, `anotacoes_complementares`, `finalizado`, `created`, `modified`, `fk_id_empresa`, `fk_id_usuario`) VALUES
+(1, 'Novo tijolo refratario', 'Este projeto é viável por conta de ...', 'Os efeitos que poderão ser notados aaaa', 'Equipe projeto 1', 'Riscos 1 aaaa', 'As datas das entregas estão previstas para aaaa', 'O projeto parte das seguintes premissas aaaa', 'Cronograma projeto 1', 'O projeto tem um custo estimado de aaaaaaaaaa', 'As empresas interessadas sao aaa aaa aaa', 'Aqui entram as anotações complementares a respeito do projeto para explicações mais profundas sobre o que será feito', 0, '2018-11-13 00:00:00', NULL, 1, 1),
+(2, 'Produto teste', 'A viabilidade deste projeto se dá por ...', 'Os efeitos que poderão ser notados BBBBbbbbBBBB', 'Equipe do projeto 2', 'Riscos 2 BBBBBB', 'As datas das entregas estão previstas para bbbbbbbbbbbb', 'O projeto parte das seguintes premissas BbBbBBbbB', 'Cronograma do projeto 2', 'O projeto tem um custo estimado de BBBBbbbbBBBBbbb', 'As empresas interessadas sao BBB bBB bbb Bbb', 'Aqui entram as anotações complementares a respeito do projeto 2 BBBBBbbBBBB', 0, '2018-11-12 22:37:15', NULL, 2, 2),
+(3, 'Teste teste teste', 'O projeto é viável porque ...', 'Os efeitos que poderão ser notados CCCcccCCCcc', 'Equipe projeto 3CC', 'Riscos 3 CccCC', 'As datas das entregas estão previstas para CCCCCCCC', 'O projeto parte das seguintes premissas CCCCC', 'Cronograma projeto 1', 'O projeto tem um custo estimado de CCCCCccccCCCC', 'As empresas interessadas sao CCCCCCC', 'Anotações complementares a respeito do projeto 3 CCCC', 0, '2018-11-12 22:39:14', NULL, 1, 3),
+(4, 'Nome do novo projeto 4', 'A viabilidade do projeto consiste em ...', 'Os efeitos que poderão ser notados 4444444', 'Equipe projeto 4 ddddd', 'Riscos 4 dddddd', 'As datas das entregas estão previstas para dddddddddddddddddddd', 'O projeto parte das seguintes premissas 44d4dd4d4', 'Cronograma do projeto 4', 'O projeto tem um custo estimado de 4444444444', 'As empresas interessadas sao 444 dddd DDD', 'Anotações e observações do projeto 4', 0, '2018-11-12 22:41:21', NULL, 3, 1),
+(5, 'Mais um produto 55', 'Este projeto é viável por conta de ...', 'Os efeitos que poderão ser notados em 55555EEE', 'Equipe do projeto 5', 'Riscos 5 EEE', 'As datas das entregas estão previstas para 5555eeee', 'O projeto parte das seguintes premissas 55555eeee', 'Cronograma projeto 5E', 'O projeto tem um custo estimado de 55EEEEEEEE', 'As empresas interessadas sao 555 eeee eeee', 'Aqui entram as anotações complementares do projeto 5E', 0, '2018-12-11 22:42:49', NULL, 1, 3),
+(6, 'Nome do projeto 6 XXXX', 'Viabilidade do projeto ...', 'Efeitos do projeto 6 FFFF', 'Equipe do projeto 6', 'Riscos do projeto 6 XXXx', 'Aqui entram as datas das entregas do projeto 6 XXXX', 'O projeto 6 parte das seguintes premissas 6666XXXXX', 'Cronograma do projeto 6x', 'O projeto 6 tem um custo estimado de 666666', 'As empresas interessadas no projeto 6 sao XXX xxx', 'Anotações e observações a respeito do projeto 6 XXX', 1, '2018-12-13 22:48:27', NULL, 2, 3),
+(7, 'Projeto 7', 'Viabilidade do Projeto 7', 'Efeitos resultantes do Projeto 7', 'Equipe do Projeto 7', 'Riscos do Projeto 7', 'Entregas do Projeto 7', 'Premissas do Projeto 7', 'Cronograma de atividades do Projeto 7', 'Custo do Projeto 7', 'Empresas interessadas no Projeto 7', 'Informações adicionais do Projeto 7', 0, '2019-01-14 22:52:37', '2019-03-13 15:06:02', 4, 4),
+(8, 'projeto 8', 'é viável', 'muitos efeitos legais', 'uma galera aí', 'um risco ou dois', 'vamos entregar', 'partimos do pressuposto que', 'tamo fazendo', 'bastante', 'algumas pessoas', 'tá bom já', 1, '2019-03-11 23:05:52', NULL, 5, 1);
 
 -- --------------------------------------------------------
 
