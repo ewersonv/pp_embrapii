caa75bf1fbf0791fe748c0398c9a6881e362e37f
ewersonv <ewersonv@gmail.com>
2019-04-26 17:05:37 -0300

    add login page + css

--- spp_ifes/view/login.php
+++ spp_ifes/view/login.php
@@ -6,24 +6,39 @@ session_start();
 	<head>
 		<meta charset="utf-8">
 		<title>SPP - Ifes</title>
+        <link href="css/login.css" rel="stylesheet" type="text/css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	</head>
-	<body>
-		<h2>Área restrita</h2>
-		<?php
+    <body>
+        
+        <?php
 			if(isset($_SESSION['msg'])){
 				echo $_SESSION['msg'];
 				unset($_SESSION['msg']);
 			}
-		?>
-		<form method="POST" action="../controller/valida.php">
-			<label>Email</label>
-			<input type="text" name="email" placeholder="Digite seu email"><br><br>
-			
-			<label>Senha</label>
-			<input type="password" name="senha" placeholder="Digite a sua senha"><br><br>
-			
-			<input type="submit" name="btnLogin" value="Acessar">
-		
-		</form>
-	</body>
+        ?>
+        
+        <div class="wrapper fadeInDown">
+            <div id="formContent">
+                 <!-- Icon -->
+                <div class="fadeIn first">
+                    <img src="https://raw.githubusercontent.com/v1eira/pp_embrapii/master/arquivos/img/LOGO_POLO-vertical-JPEG.jpg" id="icon" alt="User Icon" />
+                </div>
+                <!-- Login Form -->
+                <form method="POST" action="../controller/valida.php">
+                    <input type="text" id="login" class="fadeIn second" name="email" placeholder="Digite seu email">
+                    <input type="password" id="password" class="fadeIn third" name="senha" placeholder="Digite a sua senha">
+                    <input type="submit" class="fadeIn fourth" name="btnLogin">
+                </form>
+
+                <!-- Remind Passowrd -->
+                <div id="formFooter">
+                <a class="underlineHover" href="#">Esqueceu sua senha?</a>
+                </div>
+
+            </div>
+        </div>
+    </body>
 </html>
---
+++ spp_ifes/view/oldLogin.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>SPP - Ifes</title>
+	</head>
+	<body>
+		<h2>Área restrita</h2>
+		<?php
+			if(isset($_SESSION['msg'])){
+				echo $_SESSION['msg'];
+				unset($_SESSION['msg']);
+			}
+		?>
+		<form method="POST" action="../controller/valida.php">
+			<label>Email</label>
+			<input type="text" name="email" placeholder="Digite seu email"><br><br>
+			
+			<label>Senha</label>
+			<input type="password" name="senha" placeholder="Digite a sua senha"><br><br>
+			
+			<input type="submit" name="btnLogin">
+		
+		</form>
+	</body>
+</html>
