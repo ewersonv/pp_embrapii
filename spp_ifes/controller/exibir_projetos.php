7edb77733fbba8567b6518d5c7c15d9ebf6d42f4
v1eira <ewersonv@gmail.com>
2019-05-09 10:30:02 -0300

    att exibir_projetos.php (del echo)

--- spp_ifes/controller/exibir_projetos.php
+++ spp_ifes/controller/exibir_projetos.php
@@ -20,22 +20,16 @@ else /* se a página foi acessada via submit da página anterior */
         <div class="card border-secondary mb-3">
             <h5 class="card-header"><?php echo "<b></b>" . utf8_encode($row['nome_projeto']); ?></h5>
                 <div class="card-body">
-                    <?php
-
-                        $row_id_usuario = $row['id_usuario'];
-                        $row_finalizado = $row['finalizado'];
-                        $row_id_projeto = $row['id_projeto'];
                         
-                        echo "<b>Número do projeto: </b>" . utf8_encode($row['id_projeto']) . "<br>";
-                        echo "<b>Empresa: </b>" . utf8_encode($row['nome_empresa']) . "<br>";
-                        echo "<b>Prospectado por: </b>" . utf8_encode($row['nome_usuario']) . "<br>";
-                        echo "<b>Descrição do produto: </b>" . utf8_encode(limita_caracteres($row['descricao'], 280)) . "<br><br>";
+                        <b>Empresa: </b> <?php echo utf8_encode($row['nome_empresa']) ?> <br>
+                        <b>Prospectado por: </b> <?php echo utf8_encode($row['nome_usuario']) ?> <br>
+                        <b>Descrição do produto: </b> <?php echo utf8_encode(limita_caracteres($row['descricao'], 280)) ?> <br><br>
                         
-                        acessarProposta($tipo, $id_usuario, $row_id_usuario, $row_finalizado, $row_id_projeto);
+                        <?php acessarProposta($tipo, $id_usuario, $row['id_usuario'], $row['finalizado'], $row['id_projeto']); ?>
 
-                        echo "<a class='btn btn-sm mr-2 btn-outline-danger' href='../controller/gerar_pdf.php?id=" . $row['id_projeto'] . "' role='button'>PDF</a>";
-                        echo "<a class='btn btn-sm mr-1 btn-outline-success' href='../controller/gerar_xls.php?id=" . $row['id_projeto'] . "' role='button'>XLS</a>";
-                        ?>
+                        <a class="btn btn-sm mr-2 btn-outline-danger" href="../controller/gerar_pdf.php?id='<?php echo $row['id_projeto'] ?>'" role="button">PDF</a>
+                        <a class="btn btn-sm mr-1 btn-outline-success" href="../controller/gerar_xls.php?id='<?php echo $row['id_projeto'] ?>'" role="button">XLS</a>
+
                         <a class='btn btn-sm btn-danger' href="#" role='button' onclick="confirma(<?php echo $row['id_projeto']; ?>)">DELETAR PROPOSTA</a></p>
                         
                 </div>
