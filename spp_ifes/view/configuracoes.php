794184f3883e93f49adcc8bcb2a7c1935ec93ebe
v1eira <ewersonv@gmail.com>
2019-07-29 21:06:12 -0300

    separate css from .php

--- spp_ifes/view/configuracoes.php
+++ spp_ifes/view/configuracoes.php
@@ -6,6 +6,7 @@ include_once("header.php");
 <html id="grad1">
 <head>
     <meta charset="utf-8">
+    <link href="css/configuracoes.css" rel="stylesheet">
 </head>
 <body>
 	<div class="container">
@@ -22,7 +23,7 @@ include_once("header.php");
 			</h2>
         </div>
 
-        <div class="list-group" style="margin: auto; max-width: 70%; box-shadow: 0 10px 20px rgba(0,0,0,0.15), 0 6px 6px rgba(0,0,0,0.05);">
+        <div class="list-group">
             <a href="alterar_senha.php" class="list-group-item list-group-item-action">Alterar senha</a>
             <a href="alterar_telefone.php" class="list-group-item list-group-item-action">Alterar telefone</a>
             <?php
